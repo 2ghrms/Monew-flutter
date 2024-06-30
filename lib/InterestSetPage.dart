a3d0c83c848f714cb14af47c85bbc1cc2648e643
duathcys <duath64@soongsil.ac.kr>
2024-06-30 13:25:07 +0900

    관심사 선택 페이지 초기 설정

---
+++ lib/InterestSetPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Interestsetpage extends StatefulWidget {
+  const Interestsetpage({super.key});
+
+  @override
+  _InterestsetpageState createState() => _InterestsetpageState();
+}
+
+class _InterestsetpageState extends State<Interestsetpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
